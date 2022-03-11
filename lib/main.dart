8f535d4d230034f6ec0323ed363b9bae57637491
c151423m <dev.mryo@gmail.com>
2022-03-11 17:53:21 +0900

    final version

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,53 +18,125 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+const String _url = 'https://google.com';
 
-  final String title;
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<XFile>? _imageFileList;
+  bool isVideo = false;
+  final ImagePicker _picker = ImagePicker();
+  void _launchURL() async {
+    if (!await launch(_url)) throw 'Could not launch $_url';
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void _getImagePicker(ImageSource source, {BuildContext? context}) async {
+    try {
+      if (isVideo) {
+      } else {
+        final pickedFile = await _picker.pickImage(source: source);
+        setState(
+          () {
+            _imageFileList = pickedFile == null ? null : [pickedFile];
+          },
+        );
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  void _getMultipleImagePicker(ImageSource source,
+      {BuildContext? context}) async {
+    try {
+      final pickedMultiFile = await _picker.pickMultiImage();
+      setState(() {
+        _imageFileList = pickedMultiFile == null ? null : pickedMultiFile;
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Widget _previewImages() {
+    try {
+      if (_imageFileList != null) {
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            return Image.file(
+              File(_imageFileList![index].path),
+            );
+          },
+          itemCount: _imageFileList!.length,
+        );
+      } else {
+        return const Text("You Need to Pick an Image");
+      }
+    } catch (e) {
+      return Text(
+        e.toString(),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        centerTitle: true,
+        title: Text('Packages'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+        child: _previewImages(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              // _getImagePicker(ImageSource.gallery, context: context);
+              _getMultipleImagePicker(ImageSource.gallery, context: context);
+            },
+            child: Icon(Icons.book),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: FloatingActionButton(
+              onPressed: () {
+                isVideo = false;
+                _getImagePicker(ImageSource.camera, context: context);
+              },
+              child: Icon(Icons.camera),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: FloatingActionButton(
+              onPressed: () {
+                isVideo = false;
+                _getImagePicker(ImageSource.gallery, context: context);
+              },
+              child: Icon(Icons.image),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: FloatingActionButton(
+              onPressed: _launchURL,
+              child: Icon(Icons.network_wifi),
+            ),
+          ),
+        ],
       ),
     );
   }
